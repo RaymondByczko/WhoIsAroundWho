cf8dc4b6cabd30d022c73dc581ff157798cff765
Raymond Byczko <raymondbyczko@att.net>
2019-12-24 21:17:00 -0500

    RByczko;2019-12-24;Added namespace to JSONArchiveApi.php.

--- src/JSONArchiveApi.php
+++ src/JSONArchiveApi.php
@@ -1,7 +1,8 @@
 <?php
-	require __DIR__.'/vendor/autoload.php';
+	namespace WhoIsAroundWho;
+	require 'vendor/autoload.php';
 
-	Logger::configure(__DIR__.'/config/config.xml');
+	Logger::configure('config/config.xml');
 
 	/**
 	  * JSONArchiveApi reads a large file retrieved from the NYTimes Archive Api,
